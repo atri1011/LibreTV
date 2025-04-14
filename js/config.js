64924368f059a75ee2fe2ab27d0d9f9fa8641f7c
atri <88877231+atri1011@users.noreply.github.com>
2025-04-14 13:49:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -76,32 +76,32 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true,
+        adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true,
+        adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true,
+        adult: true
     },
     siwa: {
         api: 'https://siwazyw.tv',
         name: '丝袜资源',
-        adult: true,
+        adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true,
+        adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true,
+        adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
